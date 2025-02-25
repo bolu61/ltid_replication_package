de42d308a65392cd5ddff1111c7d4de47c9c815b
bolu61 <bolu61@zjc.dev>
2025-02-25 18:54:08 -0500

    Use package instead of install

--- ltid-toolkit/hatch_build.py
+++ ltid-toolkit/hatch_build.py
@@ -12,8 +12,8 @@ class LTIDLogGraphBuildHook(BuildHookInterface):
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         subprocess.run(
-            ["mvn", "install", "dependency:copy-dependencies", "-Dmaven.test.skip"],
-            cwd=self.src_dir,
+            ["mvn", "package", "dependency:copy-dependencies", "-Dmaven.test.skip"],
+            cwd=self.src_dir / "pkg",
         )
         jar_name = self.jar_name_template.format(version=self.metadata.version)
         build_data["force_include"][self.src_dir / "target" / "dependency"] = self.jar_dir
