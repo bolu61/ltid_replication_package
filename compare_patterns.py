8ed87530d54acfbb02369cdd1b858ef161f5a00f
bolu61 <bolu61@zjc.dev>
2025-02-25 16:48:17 -0500

    Update

--- compare_patterns.py
+++ compare_patterns.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
+from itertools import islice
 import logging
 import re
 import sys
 from argparse import ArgumentParser
-from collections.abc import Iterable
+from collections.abc import Iterator, Mapping, Sequence
 from datetime import datetime, timedelta
 from functools import partial
 from math import floor
 from pathlib import Path
-from typing import Iterator, Mapping
 
 import numpy as np
 import pandas as pd
@@ -28,14 +28,19 @@ def main(argv: list[str]):
     argument_parser = ArgumentParser()
     argument_parser.add_argument("source_tree", type=Path)
     argument_parser.add_argument("log_files", type=Path, nargs="+")
+    argument_parser.add_argument("--max_dataset_size", type=int, default=-1)
     argument_parser.add_argument("--min_sequence_length", type=int, default=2)
     argument_parser.add_argument("--max_sequence_length", type=int, default=16)
     argument_parser.add_argument("--window_size_ms", type=int, default=5)
     argument_parser.add_argument("--min_support_ratio", type=float, default=0.05)
     config = argument_parser.parse_args(argv[1:])
 
+    logger.info("building source log graph")
+
     source_log_graph = LogGraph.from_source(config.source_tree)
     loc_to_id_map = {log.loc: log.event_id for log in source_log_graph}
+
+    logger.info("parsing logs")
 
     @partial(lambda x: [*map(np.array, x())])
     def dataset():
@@ -46,14 +51,23 @@ def main(argv: list[str]):
                 logger.debug(f"in {file=}, no logs were parsed", exc_info=e)
                 return
             series = pd.Series(event_ids, index=timestamp).sort_index()
-            yield from series.rolling(
+            sequences = series.rolling(
                 timedelta(milliseconds=config.window_size_ms),
-                min_periods=config.min_sequence_length,
             )
+            if config.max_dataset_size > 0:
+                sequences = islice(sequences, config.max_dataset_size)
+            for sequence in sequences:
+                if len(sequence) < config.min_sequence_length:
+                    continue
+                yield sequence[:config.max_sequence_length]
+
+    logger.info(f"building trie with {len(dataset)} sequences")
 
     pattern_tree = make_trie(dataset, floor(config.min_support_ratio * len(dataset)))
 
     patterns_log_graph = LogGraph.from_patterns(pattern_tree)
+
+    logger.info("calculating path distance")
 
     a = {*source_log_graph.paths}
     b = {*patterns_log_graph.paths}
@@ -80,15 +94,15 @@ def parse_log(
                 dt = datetime.strptime(match.group("timestamp"), "%Y-%m-%d %H:%M:%S,%f")
                 yield dt, loc_to_id_map[loc]
             except Exception as e:
-                logger.exception(f"in {log_file=} failed to parse {log=}", exc_info=e)
+                logger.debug(f"in {log_file=} failed to parse {log=}", exc_info=e)
                 continue
 
 
-def log_graph_path_distance(a: list[int], b: list[int]) -> int:
+def log_graph_path_distance(a: Sequence[int], b: Sequence[int]) -> int:
     """calculate cyclic edit distance between two paths, without substitution"""
-    if len(a) > len(b):
+    if len(a) < len(b):
         a, b = b, a
-    return edit_distance(a + a, b, substitution_cost=2) - len(a)
+    return edit_distance([*a, *a], b, substitution_cost=2) - len(a)
 
 
 if __name__ == "__main__":
